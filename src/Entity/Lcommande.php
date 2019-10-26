fd8d063dd6900492fcb10aa770d73e9a50ce1330
PainAiko <55332444+PainAiko@users.noreply.github.com>
2019-10-26 22:15:23 +0200

    mmm

--- src/Entity/Lcommande.php
+++ src/Entity/Lcommande.php
@@ -16,78 +16,8 @@ class Lcommande
      */
     private $id;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $qte;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $tva;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Produit")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $id_produit;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Commande", inversedBy="Id_produit")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $id_commande;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getQte(): ?int
-    {
-        return $this->qte;
-    }
-
-    public function setQte(int $qte): self
-    {
-        $this->qte = $qte;
-
-        return $this;
-    }
-
-    public function getTva(): ?int
-    {
-        return $this->tva;
-    }
-
-    public function setTva(int $tva): self
-    {
-        $this->tva = $tva;
-
-        return $this;
-    }
-
-    public function getIdCommande(): ?Commande
-    {
-        return $this->id_commande;
-    }
-
-    public function setIdCommande(?Commande $id_commande): self
-    {
-        $this->id_commande = $id_commande;
-
-        return $this;
-    }
-
-    public function getIdProduit(): ?Produit
-    {
-        return $this->id_produit;
-    }
-
-    public function setIdProduit(?Produit $id_produit): self
-    {
-        $this->id_produit = $id_produit;
-
-        return $this;
     }
 }
---
+++ src/Migrations/Version20191024054425.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191024054425 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE produit ADD id_categorie_id INT NOT NULL');
+        $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC279F34925F FOREIGN KEY (id_categorie_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_29A5EC279F34925F ON produit (id_categorie_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC279F34925F');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP INDEX IDX_29A5EC279F34925F ON produit');
+        $this->addSql('ALTER TABLE produit DROP id_categorie_id');
+    }
+}
